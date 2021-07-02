4f583a45cdf3ed4c6d8754a85c239af1226dee2f
AtomGen-2 <gaonkarsvedant.471@gmail.com>
2021-07-02 18:22:36 +0530

    car detect

--- self_drive_car/Self_Drive.py
+++ self_drive_car/Self_Drive.py
@@ -1,6 +1,6 @@
 import cv2
 import random
-img_file = 'carBack.jpg'
+img_file = 'carFront.jpg'
 # Pre-trained car classfier
 classfier_file = 'car_detector.xml'
 car_tracker = cv2.CascadeClassifier(classfier_file)
@@ -15,9 +15,11 @@ numberofcars = len(car_coordinates)
 print(numberofcars)
 for i in range(0, numberofcars):
     (x, y, w, h) = car_coordinates[i]
-    cv2.rectangle(img, (x, y), (x + w, y + h), (random.randrange(128, 256), random.randrange(128, 256), random.randrange(128, 256)), 2)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 5)
 # cv2.imshow('name you want to be displayed in loaded image', img)
     cv2.imshow('CarDetector', img)
 # the image loads and immediatly closes. We do not want that, so we use the "cv2.waitKey()" to ensure that the image stays and waits until we press a key.
-    cv2.waitKey() 
+    cv2.waitKey()
+
+
 print("code completed")
